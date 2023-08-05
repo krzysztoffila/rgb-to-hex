f0bbcc32d606f0dfb5331720de714a303dbd923a
krzysztoffila <umowy.fila@gmail.com>
2023-08-05 17:46:12 +0200

    testing

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
-const redInput = document.querySelector('#red')
-const greenInput = document.querySelector('#green')
-const blueInput = document.querySelector('#blue')
-//Output
+// Pobieranie elementow
+const redInput = document.querySelector('#red');
+const greenInput = document.querySelector('#green');
+const blueInput = document.querySelector('#blue');
+const output = document.querySelector('#output');
+const div = document.querySelector('div');
+
+//funkcja
 function findHex() {
     event.preventDefault();
     let r = convert(redInput.value);
     let g = convert(greenInput.value);
     let b = convert(blueInput.value);
-    console.log(r + g + b)
+
+    let hexWithHash = `#${r}${g}${b}`;
+    output.value = hexWithHash;
 
     function convert(rgb) {
         let getRgb = Number(rgb).toString(16)
         return getRgb
     }
+    div.style.backgroundColor = hexWithHash;
+    redInput.value = ""
+    greenInput.value = ""
+    blueInput.value = ""
 }
